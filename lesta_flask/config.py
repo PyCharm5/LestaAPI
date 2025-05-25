0dd6cb72cfe83cf948a178dd479f23a1b53a9c4e
PyCharm5 <ilatelesev2@gmail.com>
2025-05-25 12:08:31 +0700

    Delete lesta_flask directory

--- lesta_flask/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    API_KEY = "1377836fd8c2ecb2382bb3dd08d1f071"
-    API_URL = "https://papi.tanksblitz.ru/wotb"
-    AUTH_URL = "https://api.tanki.su/wot/auth/login/"
-    REDIRECT_URI = "http://localhost:5000/auth"
