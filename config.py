163cecc49936dda18ac4ebc1751b0ff7c919ac05
PyCharm5 <ilatelesev2@gmail.com>
2025-05-25 13:27:20 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config:
     API_KEY = "1377836fd8c2ecb2382bb3dd08d1f071"
     API_URL = "https://papi.tanksblitz.ru/wotb"
     AUTH_URL = "https://api.tanki.su/wot/auth/login/"
-    REDIRECT_URI = "http://localhost:5000/auth"  # Измените для продакшена
+    REDIRECT_URI = "https://lestaapi.onrender.com/auth"  # Измените для продакшена
